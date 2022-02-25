91a420b18664bb40f0dd494e5e6f31e7df278e59
inaveentata <inaveentata@outlook.com>
2022-02-25 11:13:08 +0530

    Added and edited required code to the respective files repeatStrings.js and repeatStrings.spec.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, times) {
+    let result = '';
+    for (let i = 0; i < times; i++) {
+        result += string;
+    }
 
+    return times >= 0? result: 'ERROR';
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
