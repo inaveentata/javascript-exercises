cbaf8e72ae6496c50f4358ff64f9f5d5929a3e07
inaveentata <inaveentata@outlook.com>
2022-02-25 21:34:17 +0530

    Added and edited the requaried code for the two files removeFromArray.js and removeFromArray.spec.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(firstArg, ...restArg) {
+    for (let i = 0; i < restArg.length; i++) {
+        for (let j = 0; j < firstArg.length; j++) {
+            if (firstArg[j] === restArg[i]) {
+                firstArg.splice(j, 1);
+            }
+        }
+    }
+    return firstArg;
 
 };
+
+removeFromArray([1,2,3], 3,2);
 
 // Do not edit below this line
 module.exports = removeFromArray;
