9b6376b2167e8e677fb97f67fdf49ecc4dc921f3
inaveentata <inaveentata@outlook.com>
2022-02-25 12:23:56 +0530

    Added and deleted respective code for both files

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let result = '';
+    for (let i = 1; i < string.length; i++) {
+        result += string.substr(string.length-i, 1);
+    }
+    result += string.substr(0, 1);
+
+    return result;
 
 };
+
+reverseString('hello');
 
 // Do not edit below this line
 module.exports = reverseString;
