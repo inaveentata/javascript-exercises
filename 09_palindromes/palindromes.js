c6cc0b883ed1a81f6546ad72d174a14902577375
inaveentata <inaveentata@outlook.com>
2022-03-05 11:28:54 +0530

    Added requaried code and solved the exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,27 @@
-const palindromes = function () {
-    const punctuation = []
+const palindromes = function (str) {
+    const punctuation = ['!',',','.'];
+    let new_str = '';
+    for (let i = 0; i < punctuation.length; i++) {
+        for (let j = 0; j < punctuation.length; j++) {
+            if (punctuation[i] === punctuation[j]) {
+                new_str = str.replaceAll(punctuation[i], '');
+            }
+            str = new_str;
+        }
+    }
+    str = str.toLowerCase();
+    str = str.replace(/ /g, '');
+    return reverseString(str) === str? true: false;
+};
+
+const reverseString = function(string) {
+    let result = '';
+    for (let i = 1; i < string.length; i++) {
+        result += string.substr(string.length-i, 1);
+    }
+    result += string.substr(0, 1);
+
+    return result;
 };
 
 // Do not edit below this line
